2c1ca07f2e2bef58954677878f8a2a7dfe4caa31
Liam DeVoe <orionldevoe@gmail.com>
2026-01-30 15:58:07 -0500

    format

--- flake.nix
+++ flake.nix
@@ -8,7 +8,8 @@
 
   outputs = { self, nixpkgs, hegel, ... }:
     let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      supportedSystems =
+        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
     in {
       packages = forAllSystems (system:
@@ -24,53 +25,51 @@
             nativeBuildInputs = [
               pkgs.cmake
               pkgs.ninja
-              hegel.packages.${system}.default  # hegel CLI on PATH
+              hegel.packages.${system}.default # hegel CLI on PATH
             ];
 
-            cmakeFlags = lib.mapAttrsToList (k: v: lib.cmakeFeature k (toString v)) {
-              # Pre-fetched sources for FetchContent
-              FETCHCONTENT_SOURCE_DIR_REFLECTCPP = pkgs.fetchFromGitHub {
-                owner = "getml";
-                repo = "reflect-cpp";
-                tag = "v0.22.0";
-                hash = "sha256-5Og3+dM3QuCX6sT+6Rz8vwvyzQb+8qz10ROk9yOMPgE=";
-              };
+            cmakeFlags =
+              lib.mapAttrsToList (k: v: lib.cmakeFeature k (toString v)) {
+                # Pre-fetched sources for FetchContent
+                FETCHCONTENT_SOURCE_DIR_REFLECTCPP = pkgs.fetchFromGitHub {
+                  owner = "getml";
+                  repo = "reflect-cpp";
+                  tag = "v0.22.0";
+                  hash = "sha256-5Og3+dM3QuCX6sT+6Rz8vwvyzQb+8qz10ROk9yOMPgE=";
+                };
 
-              FETCHCONTENT_SOURCE_DIR_NLOHMANN_JSON = pkgs.fetchFromGitHub {
-                owner = "nlohmann";
-                repo = "json";
-                tag = "v3.12.0";
-                hash = "sha256-cECvDOLxgX7Q9R3IE86Hj9JJUxraDQvhoyPDF03B2CY=";
-              };
+                FETCHCONTENT_SOURCE_DIR_NLOHMANN_JSON = pkgs.fetchFromGitHub {
+                  owner = "nlohmann";
+                  repo = "json";
+                  tag = "v3.12.0";
+                  hash = "sha256-cECvDOLxgX7Q9R3IE86Hj9JJUxraDQvhoyPDF03B2CY=";
+                };
 
-              FETCHCONTENT_SOURCE_DIR_GOOGLETEST = pkgs.fetchFromGitHub {
-                owner = "google";
-                repo = "googletest";
-                tag = "v1.14.0";
-                hash = "sha256-t0RchAHTJbuI5YW4uyBPykTvcjy90JW9AOPNjIhwh6U=";
-              };
+                FETCHCONTENT_SOURCE_DIR_GOOGLETEST = pkgs.fetchFromGitHub {
+                  owner = "google";
+                  repo = "googletest";
+                  tag = "v1.14.0";
+                  hash = "sha256-t0RchAHTJbuI5YW4uyBPykTvcjy90JW9AOPNjIhwh6U=";
+                };
 
-              # Build options
-              HEGEL_BUILD_DOCS = "OFF";
-              HEGEL_BUILD_EXAMPLES = "OFF";
-            };
+                # Build options
+                HEGEL_BUILD_DOCS = "OFF";
+                HEGEL_BUILD_EXAMPLES = "OFF";
+              };
 
             doCheck = true;
           };
-        }
-      );
+        });
 
       devShells = forAllSystems (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
+        let pkgs = nixpkgs.legacyPackages.${system};
         in {
           default = pkgs.mkShell {
             inputsFrom = [ self.packages.${system}.default ];
             packages = [
-              pkgs.clang-tools  # clang-format, clangd
+              pkgs.clang-tools # clang-format, clangd
             ];
           };
-        }
-      );
+        });
     };
 }
--- tests/nix/flake.nix
+++ tests/nix/flake.nix
@@ -25,45 +25,40 @@
         version = "0.1.0";
         src = src;
 
-        nativeBuildInputs = [
-          pkgs.cmake
-          pkgs.ninja
-          hegel.packages.${system}.default
-        ];
+        nativeBuildInputs =
+          [ pkgs.cmake pkgs.ninja hegel.packages.${system}.default ];
 
-        cmakeFlags = lib.mapAttrsToList (k: v: lib.cmakeFeature k (toString v)) {
-          # Pre-fetched sources for FetchContent (inherited from hegel-cpp)
-          FETCHCONTENT_SOURCE_DIR_REFLECTCPP = pkgs.fetchFromGitHub {
-            owner = "getml";
-            repo = "reflect-cpp";
-            tag = "v0.22.0";
-            hash = "sha256-5Og3+dM3QuCX6sT+6Rz8vwvyzQb+8qz10ROk9yOMPgE=";
-          };
+        cmakeFlags =
+          lib.mapAttrsToList (k: v: lib.cmakeFeature k (toString v)) {
+            # Pre-fetched sources for FetchContent (inherited from hegel-cpp)
+            FETCHCONTENT_SOURCE_DIR_REFLECTCPP = pkgs.fetchFromGitHub {
+              owner = "getml";
+              repo = "reflect-cpp";
+              tag = "v0.22.0";
+              hash = "sha256-5Og3+dM3QuCX6sT+6Rz8vwvyzQb+8qz10ROk9yOMPgE=";
+            };
 
-          FETCHCONTENT_SOURCE_DIR_NLOHMANN_JSON = pkgs.fetchFromGitHub {
-            owner = "nlohmann";
-            repo = "json";
-            tag = "v3.12.0";
-            hash = "sha256-cECvDOLxgX7Q9R3IE86Hj9JJUxraDQvhoyPDF03B2CY=";
-          };
+            FETCHCONTENT_SOURCE_DIR_NLOHMANN_JSON = pkgs.fetchFromGitHub {
+              owner = "nlohmann";
+              repo = "json";
+              tag = "v3.12.0";
+              hash = "sha256-cECvDOLxgX7Q9R3IE86Hj9JJUxraDQvhoyPDF03B2CY=";
+            };
 
-          FETCHCONTENT_SOURCE_DIR_GOOGLETEST = pkgs.fetchFromGitHub {
-            owner = "google";
-            repo = "googletest";
-            tag = "v1.14.0";
-            hash = "sha256-t0RchAHTJbuI5YW4uyBPykTvcjy90JW9AOPNjIhwh6U=";
+            FETCHCONTENT_SOURCE_DIR_GOOGLETEST = pkgs.fetchFromGitHub {
+              owner = "google";
+              repo = "googletest";
+              tag = "v1.14.0";
+              hash = "sha256-t0RchAHTJbuI5YW4uyBPykTvcjy90JW9AOPNjIhwh6U=";
+            };
           };
-        };
 
         doCheck = true;
       };
 
       devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.cmake
-          pkgs.ninja
-          hegel.packages.${system}.default
-        ];
+        buildInputs =
+          [ pkgs.cmake pkgs.ninja hegel.packages.${system}.default ];
       };
     };
 }
