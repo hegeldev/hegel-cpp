ad6f96cc6f9df5a868b3232894798c02bf6db0a0
Michael Gibson <michael.gibson@antithesis.com>
2026-02-02 15:46:04 -0500

    Version = 0.1.0 and simplifying dependencies.
    Both nlohmann_json and googletest are already in pkgs.

--- flake.nix
+++ flake.nix
@@ -30,18 +30,6 @@
           tag = "v0.22.0";
           hash = "sha256-5Og3+dM3QuCX6sT+6Rz8vwvyzQb+8qz10ROk9yOMPgE=";
         };
-        nlohmann_json = pkgs.fetchFromGitHub {
-          owner = "nlohmann";
-          repo = "json";
-          tag = "v3.12.0";
-          hash = "sha256-cECvDOLxgX7Q9R3IE86Hj9JJUxraDQvhoyPDF03B2CY=";
-        };
-        googletest = pkgs.fetchFromGitHub {
-          owner = "google";
-          repo = "googletest";
-          tag = "v1.14.0";
-          hash = "sha256-t0RchAHTJbuI5YW4uyBPykTvcjy90JW9AOPNjIhwh6U=";
-        };
       };
 
       # Generate CMake flags for FetchContent sources
@@ -53,8 +41,8 @@
         in
         lib.mapAttrsToList (k: v: lib.cmakeFeature k (toString v)) {
           FETCHCONTENT_SOURCE_DIR_REFLECTCPP = deps.reflectcpp;
-          FETCHCONTENT_SOURCE_DIR_NLOHMANN_JSON = deps.nlohmann_json;
-          FETCHCONTENT_SOURCE_DIR_GOOGLETEST = deps.googletest;
+          FETCHCONTENT_SOURCE_DIR_NLOHMANN_JSON = pkgs.nlohmann_json.src;
+          FETCHCONTENT_SOURCE_DIR_GOOGLETEST = pkgs.gtest.src;
         };
 
       # export builder helper for consumers
@@ -67,7 +55,7 @@
         }@args:
         stdenv.mkDerivation {
           pname = "hegel-cpp";
-          version = "1.0.0";
+          version = "0.1.0";
           src = ./.;
 
           nativeBuildInputs = [
