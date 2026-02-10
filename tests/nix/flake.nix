1ba07d6035a02cff97e11b66f208acde780d0891
Michael Gibson <michael.gibson@antithesis.com>
2026-02-10 17:03:06 -0500

    Updated flake build and run

--- tests/nix/flake.nix
+++ tests/nix/flake.nix
@@ -16,20 +16,30 @@
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+      stdenv = pkgs.stdenv;
+      fs = pkgs.lib.fileset;
 
-      # Assemble source: tests/nix files + hegel-cpp repo at "hegel-cpp" subdir
-      src = pkgs.runCommand "nix-test-src" { } ''
-        mkdir $out
-        cp -r ${./.}/* $out/
-        cp -r ${./../..} $out/hegel-cpp
-      '';
+      test = stdenv.mkDerivation {
+          pname = "hegel-cpp-nix-test";
+          version = "0.1.0";
+          src = fs.toSource {
+            root = ./.;
+            fileset = fs.unions [ ./main.cpp ./CMakeLists.txt ];
+          };
+
+          nativeBuildInputs = [
+            pkgs.cmake
+            pkgs.ninja
+          ];
+
+          buildInputs = [
+            hegel-cpp.packages.${system}.default
+          ];
+        };
+
     in
     {
-      packages.${system}.default = hegel-cpp.lib.mkHegelCppProject {
-        inherit pkgs src;
-        pname = "hegel-cpp-nix-test";
-        version = "0.1.0";
-      };
+      packages.${system}.default = test;
 
       devShells.${system}.default = pkgs.mkShell {
         inputsFrom = [ self.packages.${system}.default ];
