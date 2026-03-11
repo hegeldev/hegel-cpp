6ae2d53263e5f6973c4e7293364eb80a36c32f3a
Ethan Chou <ethan.chou@antithesis.com>
2026-03-11 10:10:42 -0400

    temp disable release.md existence check

--- .github/scripts/release.py
+++ .github/scripts/release.py
@@ -81,16 +81,16 @@ def check(base_ref: str) -> None:
 
     release_file = ROOT / "RELEASE.md"
 
-    process = subprocess.run(
-        ["git", "cat-file", "-e", f"origin/{base_ref}:RELEASE.md"],
-        capture_output=True,
-        cwd=ROOT,
-    )
-    if process.returncode == 0:
-        raise ValueError(
-            f"RELEASE.md already exists on {base_ref}. It's possible the CI job "
-            "responsible for cutting a new release is in progress, or has failed."
-        )
+    # process = subprocess.run(
+    #     ["git", "cat-file", "-e", f"origin/{base_ref}:RELEASE.md"],
+    #     capture_output=True,
+    #     cwd=ROOT,
+    # )
+    # if process.returncode == 0:
+    #     raise ValueError(
+    #         f"RELEASE.md already exists on {base_ref}. It's possible the CI job "
+    #         "responsible for cutting a new release is in progress, or has failed."
+    #     )
 
     if not release_file.exists():
         lines = [
